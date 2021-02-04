01690c320dc1af90f7a846a0dd1bc00376db8146
hacker <renxiaoweizh@sina.com>
2021-02-04 15:19:02 +0800

    将 state 数据，将本该放在本地的，放在本地

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,9 +4,6 @@ import { connect } from 'dva';
 
 var waitedForTyped = null;
 var waitedTypedIndex = null;
-var pressButtonAudio = new Audio("https://d1490khl9dq1ow.cloudfront.net/sfx/mp3preview/ibm-computer-keyboard-press-enter-key_GJE0qjEd.mp3");
-var timer = null;
-
 class Game extends React.Component {
   constructor(props) {
 
@@ -14,38 +11,32 @@ class Game extends React.Component {
 
     this.handleClickStartButton = this.handleClickStartButton.bind(this);
     this.typing = this.typing.bind(this);
-    this.loseGame = this.loseGame.bind(this);
+
+    this.state = {
+      isGameStarted: 0,
+      score: 0,
+      word: ""
+    }
   }
 
-  calculateTime() {
-    clearInterval(timer);
+  calculateTime() {    
+    let standardSeconds = this.state.word.length;
+    if(standardSeconds < 2){
+      standardSeconds = 2;
+    }
     this.props.setRemoteState({
-      timeLeft: 5
-    });
-
-    timer = setInterval(() => {
-      var timeLeft = this.props.timeLeft - 1;
-      this.props.setRemoteState({
-        timeLeft: timeLeft
-      });
-
-      if (timeLeft <= 0) {
-        this.loseGame();
-      }
-    }, 1000);
+      timeLeft: standardSeconds
+    });   
   }
 
   handleClickStartButton(e) {
+    if (!this.state.sGameStarted) {
 
-    const { isGameStarted, setRemoteState } = this.props;
-    if (!isGameStarted) {
-      
-      setRemoteState({
+      this.setState({
         isGameStarted: 1,
         score: 0
       });
 
-      console.log("start generateWord", this.props.isGameStarted);
       this.generateWord();
       window.addEventListener("keydown", this.typing);
     }
@@ -55,7 +46,7 @@ class Game extends React.Component {
     var typed = String.fromCharCode(event.which);
     var letter = document.getElementById("letter-" + waitedTypedIndex);
     if (typed === waitedForTyped) {
-      var word = this.props.word;
+      var word = this.state.word;
       if (waitedTypedIndex !== word.length - 1) {
         letter.classList.remove("wrong");
         letter.classList.add("typed");
@@ -63,23 +54,18 @@ class Game extends React.Component {
         ++waitedTypedIndex;
         waitedForTyped = word[waitedTypedIndex];
       } else {
-        pressButtonAudio.currentTime = 0;
-        pressButtonAudio.play();
-        this.props.setRemoteState((prevState) => {
-          return { score: prevState.score + 1 };
+        this.setState({
+          score: this.state.score + 1
         });
         this.generateWord();
       }
     } else {
       letter.classList.add("wrong");
 
-      this.props.setRemoteState((prevState) => {
-        return { score: prevState.score - 1 };
+      this.setState({
+        score: this.state.score - 1
       });
 
-      if (this.props.score < 0) {
-        this.loseGame();
-      }
     }
   }
 
@@ -97,31 +83,22 @@ class Game extends React.Component {
     var random = Math.floor((Math.random() * this.props.wordList.length));
     var word = this.props.wordList[random];
 
-    this.props.setRemoteState({
+    this.setState({
       word: word
     });
+
+    var pressButtonAudio = new Audio("http://dict.youdao.com/dictvoice?audio=" + word);
+    pressButtonAudio.currentTime = 0;
+    pressButtonAudio.play();
 
     waitedTypedIndex = 0;
     waitedForTyped = word[waitedTypedIndex];
   }
 
-  loseGame() {
-    alert("man, you lose..");
-    window.removeEventListener("keydown", this.typing);
-    clearInterval(timer);
-
-    this.props.setRemoteState({
-      isGameStarted: 0
-    });
-
-    waitedForTyped = null;
-    waitedTypedIndex = null;
-  }
-
   render() {
-    
+
     // hope somebody can optimize these codes
-    const { word } = this.props;
+    const { word } = this.state;
 
     var chars = [];
     for (var i = 0; i < word.length; ++i) {
@@ -131,22 +108,16 @@ class Game extends React.Component {
       return (<span key={index} id={"letter-" + index}>{char}</span>);
     });
 
-    console.log("word_spans", word_spans)
-
     return (
       <div className="wrapper">
-        <h1>Typing Game{this.props.word}</h1>
+        <h1>Typing Game</h1>
         <p className="comment">Type fastly!</p>
-        <button style={this.props.isGameStarted ? { display: 'hidden' } : {}} onClick={this.handleClickStartButton}>START</button>
+        <button style={this.state.isGameStarted ? { display: 'none' } : {}} onClick={this.handleClickStartButton}>START</button>
 
         <div className="outer-wrap">
           <div className="row">
             <div className="score-wrap">
-              Score:<span className="score">{this.props.score}</span>
-            </div>
-
-            <div className="time-wrap">
-              Time left:<span className="time">{this.props.timeLeft}</span>
+              Score:<span className="score">{this.state.score}</span>
             </div>
           </div>
         </div>
@@ -164,16 +135,12 @@ class Game extends React.Component {
 export default connect
   (
     (state) => ({
-        isGameStarted: state.index.isGameStarted,
-        score: state.index.score,
-        word: state.index.word,
-        wordList: state.index.wordList,
-        timeLeft: state.index.timeLeft
-      }),
+      wordList: state.index.wordList
+    }),
     (dispatch) => ({
       setRemoteState(payload) {
         // dispatch(actionCreators.changeNameAction()); 不用 redux-thunk了
-        dispatch({type: 'index/setRemoteState', payload});
+        dispatch({ type: 'index/setRemoteState', payload });
       }
     })
   )(Game);
